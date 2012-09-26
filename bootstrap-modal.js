94e3d3fc00515841f22cad72f3d2dbb9b0616078
Jordan Schroter <jordan.schroter@gmail.com>
2012-09-26 16:07:02 -0400

    initial commit

---
+++ bootstrap-modal.js
@@ -0,0 +1,244 @@
+!function ($) {
+
+	"use strict"; // jshint ;_;
+	
+	/* MODAL CLASS DEFINITION
+	* ====================== */
+
+	var Modal = function (element, options) {
+		this.init(element, options);
+	}
+	
+	window.Modal = Modal;
+
+	Modal.prototype = {
+
+		constructor: Modal, 
+		
+		init: function(element, options){
+			this.options = options;
+		
+			this.$element = $(element)
+				.delegate('[data-dismiss="modal"]', 'click.dismiss.modal', $.proxy(this.hide, this));
+				
+			this.options.remote && this.$element.find('.modal-body').load(this.options.remote);
+			
+			this.options.manager && this.options.manager.appendModal(this);
+		}, 
+		
+		toggle: function () {
+			return this[!this.isShown ? 'show' : 'hide']();
+		}, 
+		
+		show: function () {
+			var that = this, 
+				e = $.Event('show.modal');
+			
+			this.$element.trigger(e);
+
+			if (e.isDefaultPrevented()) return;
+			
+			if (this.options.width){
+				this.$element.css('width', this.options.width);
+				
+				var that = this;
+				this.$element.css('margin-left', function(){
+					if (/%/ig.test(that.options.width)){
+						return -(parseInt(that.options.width) / 2) + '%';
+					} else {
+						return -($(this).width() / 2) + 'px';
+					}
+				});
+			}
+			
+			if (this.options.height){
+				this.$element.css('height', this.options.height);
+			}
+			
+			this.escape();
+			
+			this.options.loading && this.loading();
+		}, 
+		
+		hide: function (e) {
+			e && e.preventDefault();
+
+			var that = this;
+
+			e = $.Event('hide.modal');
+
+			this.$element.trigger(e);
+
+			if (!this.isShown || e.isDefaultPrevented()) return (this.isShown = false);
+
+			this.isShown = false;
+
+			this.escape();
+			
+			this.isLoading && this.loading();
+
+			$(document).off('focusin.modal');
+
+			this.$element
+				.removeClass('in')
+				.attr('aria-hidden', true);
+
+			$.support.transition && this.$element.hasClass('fade') ?
+				this.hideWithTransition() :
+				this.hideModal();
+		}, 
+		
+		escape: function () {
+			var that = this;
+			if (this.isShown && this.options.keyboard) {
+				if (!this.$element.attr('tabindex')) this.$element.attr('tabindex', -1);
+				
+				this.$element.on('keyup.dismiss.modal', function ( e ) {
+					e.which == 27 && that.hide()
+				});
+			} else if (!this.isShown) {
+				this.$element.off('keyup.dismiss.modal')
+			}
+		}, 
+		
+		hideWithTransition: function () {
+			var that = this
+				, timeout = setTimeout(function () {
+					that.$element.off($.support.transition.end)
+					that.hideModal()
+				}, 500);
+
+			this.$element.one($.support.transition.end, function () {
+				clearTimeout(timeout)
+				that.hideModal()
+			});
+		}, 
+		
+		hideModal: function () {
+			this.$element
+				.hide()
+				.trigger('hidden.modal');
+		}, 
+		
+		removeLoading: function(){
+			this.$loading.remove();
+			this.$loading = null;
+			this.isLoading = false;
+		},
+		
+		loading: function(callback){
+			var animate = this.$element.hasClass('fade') ? 'fade' : '';
+			
+			if (!this.isLoading) {
+				var doAnimate = $.support.transition && animate;
+				
+				var $parent = this.$element.find('.modal-body');
+				$parent = $parent.length ? $parent : this.$element;
+				
+				this.$loading = $('<div class="loading-mask ' + animate + '">' +
+						'<img class="loading-spinner" src="/webasp/images/oespinner.gif" />' +
+					'</div>')
+					.appendTo($parent);
+
+				if (doAnimate) this.$loading[0].offsetWidth // force reflow	
+					
+				this.$loading.addClass('in')
+
+				this.isLoading = true;
+				
+				doAnimate ?
+				this.$loading.one($.support.transition.end, callback) :
+				callback();
+
+			} else if (this.isLoading && this.$loading) {
+				this.$loading.removeClass('in');
+
+				var that = this;
+				$.support.transition && this.$element.hasClass('fade')?
+					this.$loading.one($.support.transition.end, function(){ that.removeLoading() }) :
+					that.removeLoading();
+
+			} else if (callback) {
+				callback(this.isLoading);
+			}
+		},
+		
+		toggleLoading: function(callback){ this.loading(callback); }, 
+		
+		destroy: function(){
+			var e = $.Event('destroy.modal');
+			this.$element.trigger(e);
+			if (e.isDefaultPrevented()) return;
+			
+			this.teardown();
+		},
+		
+		teardown: function(){
+			var $parent = this.$parent;
+			
+			if (!$parent.length){
+				this.$element.remove();
+				this.$element = null;
+				return;
+			}
+			
+			if ($parent !== this.$element.parent()){
+				this.$element.appendTo(this.$parent);
+			}
+			
+			this.$element.off('.modal');
+			this.$element.removeData('modal');
+			this.$element
+				.removeClass('in')
+				.attr('aria-hidden', true);
+		}
+	}
+
+
+	/* MODAL PLUGIN DEFINITION
+	* ======================= */
+
+	$.fn.modal = function (option) {
+		return this.each(function () {
+			var $this = $(this), 
+				data = $this.data('modal'), 
+				options = $.extend({}, $.fn.modal.defaults, $this.data(), typeof option == 'object' && option);
+
+			if (!data) $this.data('modal', (data = new Modal(this, options)))
+			if (typeof option == 'string') data[option]()
+			else if (options.show) data.show()
+		})
+	}
+
+	$.fn.modal.defaults = {
+		keyboard: true, 
+		backdrop: true,
+		loading: false,
+		show: true,
+		width: null,
+		manager: GlobalModalManager
+	}
+
+	$.fn.modal.Constructor = Modal
+
+
+	/* MODAL DATA-API
+	* ============== */
+	
+	$(function () {
+		$('body').off('.modal').on('click.modal.data-api', '[data-toggle="modal"]', function ( e ) {
+			var $this = $(this), 
+				href = $this.attr('href'), 
+				$target = $($this.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, ''))), //strip for ie7 
+				option = $target.data('modal') ? 'toggle' : $.extend({ remote: !/#/.test(href) && href }, $target.data(), $this.data());
+
+			e.preventDefault();
+			$target
+				.modal(option)
+				.one('hide', function () {
+					$this.focus();
+				})
+		});
+	});
+
+}(window.jQuery);
---
+++ bootstrap-modalmanager.js
@@ -0,0 +1,282 @@
+!function($){
+				
+	"use strict"; // jshint ;_;
+	
+	var $baseModal = $('<div class="modal" />').appendTo('body'),
+		$baseBackdrop = $('<div class="modal-backdrop" />').appendTo('body'),
+		$baseModalAbs = $('<div class="modal modal-absolute" />').appendTo('body'),
+		$baseBackdropAbs = $('<div class="modal-backdrop modal-absolute" />').appendTo('body');
+		
+	var baseModalzIndex = +$baseModal.css('z-index'),
+		baseModalAbszIndex = +$baseModalAbs.css('z-index'),
+		baseBackdropzIndex = +$baseBackdrop.css('z-index'),
+		baseBackdropAbszIndex = +$baseBackdropAbs.css('z-index'),
+		zIndexFactor = baseModalzIndex - baseBackdropzIndex;
+	
+	$baseModal.remove();
+	$baseBackdrop.remove();
+	$baseModalAbs.remove();
+	$baseBackdropAbs.remove();
+	$baseBackdrop = null;
+	$baseModal = null;
+	$baseModalAbs = null;
+	$baseBackdrop = null;
+	
+	/* MODAL MANAGER CLASS DEFINITION
+	* ====================== */
+	
+	var ModalManager = function (element, options) {
+		this.init(element, options);
+	}
+	
+	window.ModalManager = ModalManager;
+
+	ModalManager.prototype = {
+
+		constructor: ModalManager,
+		
+		init: function(element, options){
+			this.$element = $(element);
+			this.options = $.extend({}, $.fn.modalmanager.defaults, this.$element.data(), typeof options == 'object' && options);
+			this.stack = [];
+			this.useAbsolute = this.$element[0] !== $('body')[0];
+		},
+		
+		createModal: function(element, options){
+			$(element).modal($.extend({ manager: this }, options));
+		},
+
+		appendModal: function(modal){
+			this.stack.push(modal);
+			
+			var that = this;
+			
+			modal.$element.on('show.modal', function(e){
+				modal.isShown = true;
+				$('body').toggleClass('modal-open', that.hasOpenModal());
+				
+				modal.$element
+					.toggleClass('modal-absolute', that.useAbsolute)
+					.css('margin-top', that.$element.scrollTop()) //TODO: handle container overflow scroll when it is not body 
+					.css('z-index', (that.useAbsolute ? baseModalAbszIndex : baseModalzIndex) 
+						+ (zIndexFactor * that.getIndexOfModal(modal)));
+					
+				that.backdrop(modal, function () {
+					var transition = $.support.transition && modal.$element.hasClass('fade')
+
+					modal.$parent =  modal.$element.parent();
+					if (!modal.$parent.length || modal.$parent[0] !== that.$element[0]) {
+						modal.$element.appendTo(that.$element);
+					}
+
+					modal.$element.show();
+
+					if (transition) {
+						modal.$element[0].style.display = 'run-in'; 
+						modal.$element[0].offsetWidth;
+						modal.$element.one($.support.transition.end, function () { modal.$element[0].style.display = 'block' });
+					}
+
+					modal.$element
+						.addClass('in')
+						.attr('aria-hidden', false);
+
+					transition ?
+						modal.$element.one($.support.transition.end, function () { modal.$element.trigger('shown.modal') }) :
+						modal.$element.trigger('shown.modal');
+				})
+				
+				modal.$element.on('shown.modal', function(e){
+					that.setFocus();
+				})
+				
+			});
+			
+			modal.$element.on('hidden.modal', function(e){
+				that.backdrop(modal);
+
+				if (modal.$backdrop){
+					$.support.transition && modal.$element.hasClass('fade')?
+						modal.$backdrop.one($.support.transition.end, function(){ that.destroyModal(modal) }) :
+						that.destroyModal(modal);
+				} else {
+					that.destroyModal(modal);
+				}
+				
+			});
+			
+			modal.$element.on('destroy.modal', function(e){
+				that.removeModal(modal);
+			});
+		},
+		
+		destroyModal: function(modal){
+			modal.destroy();
+			$('body').toggleClass('modal-open', this.hasOpenModal());
+			this.setFocus();
+		},
+		
+		hasOpenModal: function(){
+			for (var i = 0; i < this.stack.length; i++){
+				if (this.stack[i].isShown) return true;
+			}
+			return false;
+		},
+		
+		setFocus: function () {
+			var topModal;
+			
+			for (var i = 0; i < this.stack.length; i++){
+				if (this.stack[i].isShown) topModal = this.stack[i];
+			}
+			
+			topModal && topModal.$element.focus();
+		},
+
+		removeModal: function(modal){
+			if (modal.$backdrop) this.removeBackdrop.call(modal);
+			this.stack.splice(this.getIndexOfModal(modal), 1);
+		},
+		
+		getModalAt: function(index){
+			return this.stack[index];
+		},
+		
+		getIndexOfModal: function(modal){
+			for (var i = 0; i < this.stack.length; i++){
+				if (modal === this.stack[i]) return i;
+			}
+		},
+		
+		removeBackdrop: function (modal) {
+			modal.$backdrop.remove();
+			modal.$backdrop = null;
+		}, 
+		
+		createBackdrop: function(animate){
+			var $backdrop;
+			
+			if (!this.isLoading) {
+				$backdrop =  $('<div class="modal-backdrop ' + animate + '" />')
+					.appendTo(this.$element)
+					.toggleClass('modal-absolute', this.useAbsolute);
+			} else {
+				$backdrop = this.$loading;
+				$backdrop.off('.modalmanager').html('');
+				this.$loading = null;
+				this.isLoading = false;
+			}
+				
+			return $backdrop
+		},
+		
+		backdrop: function (modal, callback) {
+			var animate = modal.$element.hasClass('fade') ? 'fade' : '',
+				showBackdrop = $.extend({}, this.options, modal.options).backdrop;
+				
+			if (modal.isShown && showBackdrop) {
+				var doAnimate = $.support.transition && animate && !this.isLoading;
+
+
+				modal.$backdrop = this.createBackdrop(animate);
+				
+				modal.$backdrop.css('z-index', 
+						(this.useAbsolute ? baseBackdropAbszIndex : baseBackdropzIndex) 
+						+ (zIndexFactor * this.getIndexOfModal(modal)));
+			
+				
+				if (modal.options.backdrop != 'static') {
+					modal.$backdrop.on('click.modal', $.proxy(modal.hide, modal));
+				}
+
+				if (doAnimate) modal.$backdrop[0].offsetWidth // force reflow
+
+				modal.$backdrop.addClass('in')
+
+				doAnimate ?
+				modal.$backdrop.one($.support.transition.end, callback) :
+				callback();
+
+			} else if (!modal.isShown && modal.$backdrop) {
+				modal.$backdrop.removeClass('in');
+
+				var that = this;
+				$.support.transition && modal.$element.hasClass('fade')?
+					modal.$backdrop.one($.support.transition.end, function(){ that.removeBackdrop(modal) }) :
+					that.removeBackdrop(modal);
+
+			} else if (callback) {
+				callback();
+			}
+		},
+		
+		removeLoading: function(){
+			this.$loading.remove();
+			this.$loading = null;
+			this.isLoading = false;
+		},
+		
+		loading: function(callback){
+			if (!this.isLoading) {
+			
+				this.$loading = this.createBackdrop('fade');
+
+				this.$loading[0].offsetWidth // force reflow	
+					
+				this.$loading
+					.css('z-index', 
+						(this.useAbsolute ? baseBackdropAbszIndex : baseBackdropzIndex) 
+						+ (zIndexFactor * this.stack.length))
+					.append('<img class="loading-spinner" src="/webasp/images/oespinner.gif" />')
+					.on('click.modalmanager', $.proxy(this.loading, this))
+					.addClass('in');
+
+				this.isLoading = true;
+				
+				$.support.transition ?
+				this.$loading.one($.support.transition.end, callback) :
+				callback();
+
+			} else if (this.isLoading && this.$loading) {
+				this.$loading.removeClass('in');
+
+				var that = this;
+				$.support.transition ?
+					this.$loading.one($.support.transition.end, function(){ that.removeLoading() }) :
+					that.removeLoading();
+
+			} else if (callback) {
+				callback(this.isLoading);
+			}
+		},
+		
+		toggleLoading: function(callback){ this.loading(callback); }
+	}
+
+
+	/* MODAL MANAGER PLUGIN DEFINITION
+	* ======================= */
+
+	$.fn.modalmanager = function (option) {
+		return this.each(function () {
+			var $this = $(this)
+			, data = $this.data('modalmanager');
+
+			if (!data) $this.data('modalmanager', (data = new ModalManager(this, options)))
+			if (typeof option == 'string') data[option]()
+		})
+	}
+
+	$.fn.modalmanager.defaults = {
+		backdrop: true
+	}
+
+	$.fn.modalmanager.Constructor = ModalManager
+	
+	// Create a default global modal manager 
+	window.GlobalModalManager = new ModalManager('body');
+	
+}(jQuery);
+
+
+
